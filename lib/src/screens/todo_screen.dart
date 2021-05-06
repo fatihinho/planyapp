948b8741498a8cc0913319d9af83cbfa90b3abe6
Fatih Çınar <fcinar_38@hotmail.com>
2021-05-06 22:48:53 +0300

    added bottom nav bar

--- lib/src/screens/todo_screen.dart
+++ lib/src/screens/todo_screen.dart
@@ -11,40 +11,53 @@ class TodoScreen extends StatelessWidget {
     var screenWidth = size.width;
 
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.cyan,
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.indigo,
-        ),
-        body: Stack(
-          children: [
-            Container(
-              color: Colors.white,
-            ),
-            Container(
-              height: screenHeight * 0.12,
-              width: screenWidth,
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'Kişisel Planlar $_title',
-                  style: TextStyle(
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.indigo,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            color: Colors.indigo,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Kişisel Planlar $_title',
+                style: TextStyle(
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
               ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: screenHeight * 0.68,
               decoration: BoxDecoration(
-                  color: Colors.indigo,
+                  color: Colors.white,
                   borderRadius: BorderRadius.only(
-                      bottomRight: Radius.circular(30.0),
-                      bottomLeft: Radius.circular(30.0))),
-            )
-          ],
-        ));
+                      topLeft: Radius.circular(30.0),
+                      topRight: Radius.circular(30.0))),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.cyan,
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      bottomNavigationBar:
+          BottomNavigationBar(backgroundColor: Colors.white, items: [
+        BottomNavigationBarItem(label: 'Bugün', icon: Icon(Icons.today)),
+        BottomNavigationBarItem(
+            label: 'Hafta', icon: Icon(Icons.calendar_view_day_outlined)),
+        BottomNavigationBarItem(
+            label: 'Ay', icon: Icon(Icons.calendar_today_rounded)),
+      ]),
+    );
   }
 }
