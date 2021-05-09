14a5a4fda6e87a19191e32fa640dac3d13295b50
Fatih Çınar <fcinar_38@hotmail.com>
2021-05-10 02:58:04 +0300

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:planyapp/src/providers/task_provider.dart';
 import 'package:planyapp/src/screens/home_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(App());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: App(),
+    ),
+  );
 }
 
 class App extends StatelessWidget {
