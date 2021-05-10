bdbc852bec921f895e7ca04e960a3c6c505d109c
Fatih Çınar <fcinar_38@hotmail.com>
2021-05-10 03:00:58 +0300

    added textstyles another class

---
+++ lib/src/widgets/textstyles_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class TasksTextStyles {
+  static Icon get uncompletedTaskLeading =>
+      Icon(Icons.check_box_outline_blank, size: 36.0);
+  static Icon get completedTaskLeading => Icon(
+        Icons.check_box_rounded,
+        color: Colors.cyan,
+        size: 36.0,
+      );
+  static TextStyle get uncompletedTitleTextStyle => TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey[800],
+      decoration: TextDecoration.none);
+  static TextStyle get completedTitleTextStyle => TextStyle(
+      fontSize: 18.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.grey,
+      decoration: TextDecoration.lineThrough);
+  static TextStyle get uncompletedNoteStyle =>
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.blueGrey[400]);
+  static TextStyle get completedNoteStyle => TextStyle(
+      fontWeight: FontWeight.normal,
+      color: Colors.grey,
+      decoration: TextDecoration.lineThrough);
+  static TextStyle get uncompletedDateTimeStyle =>
+      TextStyle(fontWeight: FontWeight.bold, color: Colors.brown);
+  static TextStyle get completedDateTimeStyle => TextStyle(
+      fontWeight: FontWeight.normal,
+      color: Colors.grey,
+      decoration: TextDecoration.lineThrough);
+}
